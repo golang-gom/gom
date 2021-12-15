265b60319f804cbc0bfa02e8a263f8dc7e832995
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-12-15 13:24:17 +0700

    feat(gom): attr build

--- attribute.go
+++ attribute.go
@@ -1,5 +1,7 @@
 package gom
 
+import "fmt"
+
 type Attribute struct {
 	Name  string
 	Value string
@@ -7,4 +9,9 @@ type Attribute struct {
 
 func Attr(name string, value string) Attribute {
 	return Attribute{name, value}
+}
+
+func (a Attribute) Build() (attrs string) {
+	attrs = fmt.Sprintf("%s=\"%s\"", a.Name, a.Value)
+	return
 }
