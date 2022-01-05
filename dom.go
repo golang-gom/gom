407a327195330ddd957afec6c90ef245d27d4334
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-01-05 14:47:31 +0700

    fix(gom): DOM utils as anonymous func

--- dom.go
+++ dom.go
@@ -1,27 +1,27 @@
 package gom
 
 var (
-	HTML = func() *Element { return H("html") }()
-	Head = func() *Element { return H("head") }()
-	Body = func() *Element { return H("body") }()
-	Div  = func() *Element { return H("div") }()
+	HTML = func() *Element { return H("html") }
+	Head = func() *Element { return H("head") }
+	Body = func() *Element { return H("body") }
+	Div  = func() *Element { return H("div") }
 
-	Table = func() *Element { return H("table") }()
-	THead = func() *Element { return H("thead") }()
-	TBody = func() *Element { return H("tbody") }()
-	TR    = func() *Element { return H("tr") }()
-	TD    = func() *Element { return H("td") }()
+	Table = func() *Element { return H("table") }
+	THead = func() *Element { return H("thead") }
+	TBody = func() *Element { return H("tbody") }
+	TR    = func() *Element { return H("tr") }
+	TD    = func() *Element { return H("td") }
 
-	Title = func() *Element { return H("title") }()
-	H1    = func() *Element { return H("h1") }()
-	H2    = func() *Element { return H("h2") }()
-	H3    = func() *Element { return H("h3") }()
-	H4    = func() *Element { return H("h4") }()
-	H5    = func() *Element { return H("h5") }()
-	Span  = func() *Element { return H("span") }()
-	I     = func() *Element { return H("i") }()
-	B     = func() *Element { return H("b") }()
-	U     = func() *Element { return H("u") }()
+	Title = func() *Element { return H("title") }
+	H1    = func() *Element { return H("h1") }
+	H2    = func() *Element { return H("h2") }
+	H3    = func() *Element { return H("h3") }
+	H4    = func() *Element { return H("h4") }
+	H5    = func() *Element { return H("h5") }
+	Span  = func() *Element { return H("span") }
+	I     = func() *Element { return H("i") }
+	B     = func() *Element { return H("b") }
+	U     = func() *Element { return H("u") }
 )
 
 func Text(text string) *Element {
